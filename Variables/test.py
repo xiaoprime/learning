04cdc56bff2d4dab422739af33419a32f70d4f72
Prime <31033455+xiaoprime@users.noreply.github.com>
2018-04-05 17:05:53 +0800

    re-arch
    One Topic One Folder

--- Variables/test.py
+++ Variables/test.py
@@ -2,20 +2,15 @@ import tensorflow as tf
 
 #tf.enable_eager_execution()
 
-print(tf.executing_eagerly())       # => True
+print(tf.executing_eagerly())
 
 tf_variable = tf.Variable([[0.1]], tf.float32)
 random = tf.random_normal([1,1])
 add = tf.assign(tf_variable, tf.add(tf_variable, random))
 print(add)
 
-#constant = tf.constant([1, 2, 3])
 init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
 	sess.run(init_op)
 	for _ in range(3):
-		print(sess.run(add))
 		print(sess.run(tf_variable))
-	#print(constant.eval())
-
-#print("hello, {}".format(m.eval()))
