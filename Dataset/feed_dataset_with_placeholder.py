d437a6664a9a1f340ffec91df92cc3a7bd36401d
Prime <31033455+xiaoprime@users.noreply.github.com>
2018-04-05 18:17:47 +0800

    Add "feed dataset with placeholder"
    A dynamical way to reduce the size of memory, compared with initialize by tf.constant().

--- Dataset/feed_dataset_with_placeholder.py
+++ Dataset/feed_dataset_with_placeholder.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 import numpy as np
 
 # Load the training data into two NumPy arrays, for example using `np.load()`.
-	# Use fake data
-with np.load("/var/data/training_data.npy") as data:
-  features = data["features"]
-  labels = data["labels"]
+# Use fake data
+#with np.load("/var/data/training_data.npy") as data:
+features = np.array([1,2,3])
+labels = np.array([2,3,4])
 
 # Assume that each row of `features` corresponds to the same row as `labels`.
 assert features.shape[0] == labels.shape[0]
@@ -15,8 +15,17 @@ labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
 
 dataset = tf.data.Dataset.from_tensor_slices((features_placeholder, labels_placeholder))
 # [Other transformations on `dataset`...]
-dataset = ...
+# dataset = ...
 iterator = dataset.make_initializable_iterator()
 
-sess.run(iterator.initializer, feed_dict={features_placeholder: features,
-                                          labels_placeholder: labels})
+next_element = iterator.get_next()
+
+with tf.Session() as sess:
+	sess.run(iterator.initializer, feed_dict={features_placeholder: features, labels_placeholder: labels})
+	print(dataset)
+	while True:
+		try:
+  			print(sess.run(next_element))
+		except tf.errors.OutOfRangeError:
+  			print("End of dataset")  # ==> "End of dataset"
+  			break
