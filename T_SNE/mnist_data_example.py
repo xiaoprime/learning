49f2159ad563e12e408e707692908c899aad8069
Prime <31033455+xiaoprime@users.noreply.github.com>
2018-04-10 15:32:42 +0800

    Fix bug
    now can run tensorboard to see T_SNE

--- T_SNE/mnist_data_example.py
+++ T_SNE/mnist_data_example.py
@@ -34,7 +34,7 @@ with tf.Session() as sess:
     # Link this tensor to its metadata file (e.g. labels).
     embedding.metadata_path = metadata
     # Saves a config file that TensorBoard will read during startup.
-    projector.visualize_embeddings(tf.summary.FileWriter(TENSORBOARD_FOLDER), config)
+    projector.visualize_embeddings(tf.summary.FileWriter(TENSORBOARD_FOLDER, sess.graph), config)
     
 # for calling the tensorboard you should be in that drive and call the entire path
 #tensorboard --logdir=/Technical_works/tensorflow/mnist-tensorboard/log-1 --port=6006
